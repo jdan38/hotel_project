ac1ad042ebcea26b58ab0bf8b4613ed9a96cf009
jdan38 <35674798+jdan38@users.noreply.github.com>
2021-04-07 22:12:02 -0400

    connected to DB

--- hotel/settings.py
+++ hotel/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
+    'listings.apps.ListingsConfig',
+    'staff.apps.StaffConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +78,11 @@ WSGI_APPLICATION = 'hotel.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hjddb',
+        'USER': 'postgres',
+        'PASSWORD': '3edc$Rfv',
+        'HOST': 'localhost'
     }
 }
 
