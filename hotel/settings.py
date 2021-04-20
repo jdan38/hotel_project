3dd1595f1fcf85bb50ab3a2398a0bf88bfcb3c22
jdan38 <35674798+jdan38@users.noreply.github.com>
2021-04-19 23:22:01 -0400

    Navbar update

--- hotel/settings.py
+++ hotel/settings.py
@@ -4,10 +4,10 @@ from secrets import *
 from pathlib import Path
 from dotenv import load_dotenv
 
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
+# env = environ.Env(
+#     # set casting, default value
+#     DEBUG=(bool, True)
+# )
 # reading .env file
 environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY =['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
