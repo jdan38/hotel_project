555eb3f2208126c8b3514ed43d74897b953f0859
jdan38 <35674798+jdan38@users.noreply.github.com>
2021-04-25 11:41:05 -0400

    make changes to listing db

--- staff/admin.py
+++ staff/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
+
 from .models import Staff
 
-admin.site.register(Staff)
+class StaffAdmin(admin.ModelAdmin):
+    list_display = ('id', 'fname', 'lname','phone', 'email', 'is_eom')
+    list_filter = ('id', 'fname', 'lname')
+    list_editable = ('phone', 'is_eom')
+   
+
+admin.site.register(Staff, StaffAdmin)
